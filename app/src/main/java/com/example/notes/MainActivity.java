43467e21dfcab7aaf46e333fc77e5a938db5e10a
Shravan Shetty <shsh9888@colorado.edu>
2020-05-28 17:38:48 -0600

    Initialization of Menu

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.add_note) {
+            Intent intent= new Intent(getBaseContext(), AddNote.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+}
