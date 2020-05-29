874e60b94d5e1530aeb71c93f0953f56cd87ca83
Shravan Shetty <shsh9888@colorado.edu>
2020-05-29 01:10:01 -0600

    Storage feature added end to end

--- app/src/main/java/com/example/notes/AddNote.java
+++ app/src/main/java/com/example/notes/AddNote.java
@@ -2,13 +2,59 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 
 public class AddNote extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    String noteString;
+    ArrayList<String> notes = new ArrayList<String>();
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+
+        noteString = sharedPreferences.getString("myNotes", "");
+        String[] notesArr  = noteString.split(",");
+        for(String not : notesArr) {
+            if (not != ""){
+                notes.add(not);
+            }
+        }
+
+        TextView noteText  = (TextView) findViewById(R.id.addNotetext);
+        intent = getIntent();
+        noteText.setText(intent.getStringExtra("note"));
+
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        TextView noteText  = (TextView) findViewById(R.id.addNotetext);
+        String newNote = noteText.getText().toString();
+
+        if (!newNote.equals("")) {
+            //Add it to storage
+            int position = intent.getIntExtra("position", 0);
+            Log.i("Position entered", position + "");
+            notes.set(position, newNote);
+            String serializedNotes = notes.toString();
+            serializedNotes = serializedNotes.substring(1, serializedNotes.length()-1);
+            sharedPreferences.edit().putString("myNotes", serializedNotes).apply();
+        }
     }
 }
--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -7,30 +7,97 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
-    ArrayList<String> notes;
+    ArrayList<String> notes= new ArrayList<String>();
+    boolean shouldOnResumeBeCalled = false;
+    ArrayAdapter<String> notesAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        shouldOnResumeBeCalled=false;
         setContentView(R.layout.activity_main);
-        notes = new ArrayList<String>();
-        notes.add("Empty Note");
+        sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+//        sharedPreferences.edit().remove("myNotes").apply();
+//        sharedPreferences.edit().clear().apply();
+
+
+        String noteString = sharedPreferences.getString("myNotes", "");
+        String[] notesArr  = noteString.split(",");
+        for(String not : notesArr) {
+            if (not != ""){
+                notes.add(not);
+            }
+        }
+
+
+        if (notes.size() == 0) {
+            notes.add("Empty Note");
+            String serializedNotes = notes.toString();
+            serializedNotes = serializedNotes.substring(1, serializedNotes.length()-1);
+            sharedPreferences.edit().putString("myNotes", serializedNotes).apply();
+        }
+        Log.i("stored", noteString.toString());
+
 
         ListView listView = findViewById(R.id.noteList);
-        ArrayAdapter<String> notesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
+        notesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(notesAdapter);
 
-        sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent= new Intent(getBaseContext(), AddNote.class);
+                intent.putExtra("note", notes.get(position));
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                notes.remove(position);
+                notesAdapter.notifyDataSetChanged();
+                return true;
+            };
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (shouldOnResumeBeCalled) {
+            sharedPreferences = this.getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+            String noteString = sharedPreferences.getString("myNotes", "");
+            String[] notesArr  = noteString.split(",");
+            notes.clear();
+            for(String not : notesArr) {
+                if (not != ""){
+                    notes.add(not);
+                }
+            }
+            Log.i("stored2", noteString.toString());
+            notesAdapter.notifyDataSetChanged();
+        } else {
+            shouldOnResumeBeCalled = true;
+        }
+
 
     }
 
@@ -38,7 +105,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
-
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -47,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         super.onOptionsItemSelected(item);
         if (item.getItemId() == R.id.add_note) {
             Intent intent= new Intent(getBaseContext(), AddNote.class);
+            intent.putExtra("position", -1);
             startActivity(intent);
         }
         return true;
